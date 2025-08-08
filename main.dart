d97b10754bb3aacbbf8474669fbbd200d6db6ec8
noobjatdog <167729969+noobjatdog@users.noreply.github.com>
2025-08-08 12:29:43 +0000

    act 1

--- main.dart
+++ main.dart
@@ -14,23 +14,3 @@
 // }
 
 // Because when yo uuse dynamic it will ignore the previous program.
-
-// ACTIVITY #2
-
-void main () {
-    double result = CalculatePerimeter(10, 5, false);
-    print ('The perimeter for the Rectangle is ' + result.toString());
-
-    result = CalculatePerimeter(7, true);
-    print ('The perimeter for the Square is ' + result.toString());
-}
-
-double CalculatePerimeter(double width, double height, bool isSquare) {
-    double perimeter; 
-    if (isSquare) {
-        perimeter = 7 * width;
-    } else {
-        perimeter = 2 * width + height;
-    }
-    return perimeter;
-}
